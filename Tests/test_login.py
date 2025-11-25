99bd235dfcbd673da73dd418c590398719101d92
dbasnet <deeptibasnet15@gmail.com>
2025-11-25 10:58:47 +0545

    test folder

---
+++ Tests/test_login.py
@@ -0,0 +1,31 @@
+from _pytest.config import Config
+import pytest
+from Pages.Login.login_page import LoginPage
+from Tests.base_test import BaseTest
+from Pages.Login.Login_Props import TestData
+
+
+
+
+@pytest.mark.usefixtures("setup")
+class TestFoodManduLoginPage(BaseTest):
+    def test_login_with_valid_credentials(self):
+        # Open website using BaseTest method
+        self.open_url(TestData.BASE_URL)
+
+        # Validate page title
+        assert "foodmandu" in self.get_page_title().lower(), \
+            f"Expected 'foodmandu' in title, got {self.get_page_title()}"
+
+        # Login flow
+        login_page = LoginPage(self.driver)
+        login_page.click_login_FORM()  # If login requires popup
+        login_page.enter_email(TestData.EMAIL)
+        login_page.enter_password(TestData.PASSWORD)
+        login_page.click_login()
+
+        # Take screenshot after login
+        self.take_screenshot("login_success")
+
+        # Optional: Assert current URL after login
+        assert "foodmandu.com" in self.driver.current_url
---
+++ Tests/test_login_negative.py
@@ -0,0 +1,43 @@
+import time
+from asyncio import wait_for
+
+import  pytest
+import selenium
+from selenium.webdriver.common.by import By
+from Pages.Login.login_page import LoginPage
+from Tests.base_test import BaseTest
+from Pages.Login.Login_Props import TestData
+
+@pytest.mark.usefixtures("setup")
+class TestLoginNegative(BaseTest):
+    def test_login_invalidPassword(self):
+        self.open_url(TestData.BASE_URL)
+        driver = self.driver
+        login_page = LoginPage(driver)
+        login_page.click_login_FORM()
+        login_page.enter_email(TestData.EMAIL)
+        login_page.enter_password(TestData.Invalid_Password)
+        self.take_screenshot("Invalid Password")
+        login_page.click_login()
+        time.sleep(5)
+
+
+        assert "foodmandu.com" in self.driver.current_url
+        error_text = login_page.get_error_message()
+        assert "Invalid login attempt" in error_text, f"Unexpected error message: {error_text}"
+
+    def test_login_invalidEmail(self):
+        self.open_url(TestData.BASE_URL)
+        driver = self.driver
+        login_page = LoginPage(driver)
+        login_page.click_login_FORM()
+        login_page.enter_email(TestData.Invalid_Email)
+        login_page.enter_password(TestData.PASSWORD)
+        self.take_screenshot("Invalid Email")
+        login_page.click_login()
+        time.sleep(5)
+
+
+        assert "foodmandu.com" in self.driver.current_url
+        error_text = login_page.get_error_message()
+
